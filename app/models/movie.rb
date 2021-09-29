1fe1557a5930c3fa45d7a4ee9239fafa01043683
EC2 Default User <ec2-user@ip-172-31-7-89.us-east-2.compute.internal>
2021-09-29 20:07:18 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,3 @@
 class Movie < ActiveRecord::Base
-    def self.rate
-        rate=Array.new
-        self.select("rating").uniq.each {|x| rate.push(x.rating)}
-        rate.sort.uniq
-    end
-    
-    def self.with_ratings(rating_list)
-        if rating_list == nil
-            self.rate
-        end
-        self.where("rating":rating_list)
-    end
+   
 end
